432c1e07e7c62162663efecda48560d23874b0ac
Your Name <your.email@example.com>
2026-02-05 18:06:24 +0100

    feat: add and connect checkout, cart, shipping, payment, and thank you pages

--- client/src/routes.tsx
+++ client/src/routes.tsx
@@ -2,14 +2,18 @@ import Home from './pages/Home';
 import Marketplace from './pages/Marketplace';
 import Auctions from './pages/Auctions';
 import Drop from './pages/Drop';
-import ProductPage from './marketplace/ProductPage'; 
+import ProductPage from './marketplace/ProductPage';
 import LiveBid from './auctions/livebid';
+import CartPage from './marketplace/CartPage';
+import ThankYou from './marketplace/ThankYou';
 
 export const appRoutes = [
   { path: '/', element: <Home /> },
   { path: '/Marketplace', element: <Marketplace /> },
   { path: '/Auctions', element: <Auctions /> },
   { path: '/Drop', element: <Drop /> },
-  { path: '/Marketplace/product/:id', element: <ProductPage /> }, 
+  { path: '/Marketplace/product/:id', element: <ProductPage /> },
   { path: '/livebid', element: <LiveBid /> },
+  { path: '/Marketplace/Cart', element: <CartPage /> },
+  { path: '/Marketplace/ThankYou', element: <ThankYou /> },
 ];
