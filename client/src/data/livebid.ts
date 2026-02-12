5474e85a2be9b25a0e31fb3d3f9558f2871d453a
Tobiloba Ayomide <oyetunjitobiloba82@gmail.com>
2026-02-12 01:48:38 +0100

    feat: add live bid messages with profile images

---
+++ client/src/data/livebid.ts
@@ -0,0 +1,45 @@
+import livebidProfile1 from "../assets/livebid/1.png";
+import livebidProfile2 from "../assets/livebid/2.png";
+import livebidProfile3 from "../assets/livebid/3.png";
+import livebidProfile4 from "../assets/livebid/4.png";
+import livebidProfile5 from "../assets/livebid/5.png";
+import livebidProfile6 from "../assets/livebid/6.png";
+
+export const livebidMessages = [
+  {
+    id: 1,
+    profileImg: livebidProfile1,
+    name: "Sammy Ellen",
+    message: "i love this, $20 for me",
+  },
+  {
+    id: 2,
+    profileImg: livebidProfile2,
+    name: "Celestina Quinn",
+    message: "gm frens! ready to bidddd",
+  },
+  {
+    id: 3,
+    profileImg: livebidProfile3,
+    name: "Opeyemi Tiwalope",
+    message: "$45 is my bid!",
+  },
+  {
+    id: 4,
+    profileImg: livebidProfile4,
+    name: "Uncle Luca",
+    message: "Instant bid",
+  },
+  {
+    id: 5,
+    profileImg: livebidProfile5,
+    name: "Ella Flynn",
+    message: "tight bid $30",
+  },
+  {
+    id: 6,
+    profileImg: livebidProfile6,
+    name: "Henry Cavill",
+    message: "$45.00 instant bid",
+  },
+];
