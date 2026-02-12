8d6fa5d19cb1072b8fc929f9185b0a94a3edad95
Tobiloba Ayomide <oyetunjitobiloba82@gmail.com>
2026-02-12 01:49:58 +0100

    feat: add product and auction card interfaces

--- client/src/data/types/home.model.ts
+++ client/src/data/types/home.model.ts
@@ -1,5 +1,26 @@
+import type { products } from "..";
+import type { AuctionCardT } from "./auction.model";
+
 export type HeroImageT = {
   src: string;
   style?: React.CSSProperties;
   rotate?: string;
 };
+
+export interface ProductCardProps {
+  product: typeof products[0];
+  index: number;
+  onViewProduct?: () => void;
+  onSeeCreators?: () => void;
+}
+
+export interface ProductImageHoverProps {
+  src: string;
+  onViewClick?: () => void;
+}
+
+export interface AuctionCardProps {
+  card: AuctionCardT;
+  onSeeMore?: () => void;
+  onSetReminder?: () => void;
+}
