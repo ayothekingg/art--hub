ba7aa5cd080780628495c06ebc2c4a832a25cd67
ayothekingg <ayojames444@gmail.com>
2026-02-17 16:45:34 +0100

    fix: index.js file fixed after merge conflict

--- server/index.js
+++ server/index.js
@@ -1,32 +1,20 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
-import { connectDB } from "../server/src/config/db.js";
-import authRoutes from "../server/src/routes/auth.routes.js";
+import { connectDB } from "./src/config/db.js";
+import authRoutes from "./src/routes/auth.routes.ts";
+import productRoutes from "./src/routes/product.routes.ts";
 
 dotenv.config();
 connectDB();
 
 const app = express();
-
-// Allow requests only from your React app's origin
-app.use(cors({
-  origin: "http://localhost:5173",
-  credentials: true
-}));
-
+app.use(cors());
 app.use(express.json());
-app.use("api/auth", authRoutes);
+app.use("/api/auth", authRoutes);
+app.use("/api/product", productRoutes);
 app.get("/", (req, res) => {
   res.send("Art eCommerce API is running x");
 });
 
-
-    app.listen(4000, () => console.log("Server running on port 4000"));
-  } catch (err) {
-    console.error("Failed to connect to MongoDB:", err);
-    process.exit(1);
-  }
-}
-
-startServer();
+app.listen(5001, () => console.log("Server running on port 5001"));
