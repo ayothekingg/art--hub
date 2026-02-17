608eebdbb729508719cc62f8e20e94a25877d27a
ayothekingg <ayojames444@gmail.com>
2026-02-17 16:44:53 +0100

    refactor: file name change to .ts extension

--- server/src/middleware/auth.middleware.ts
+++ server/src/middleware/auth.middleware.ts
@@ -4,12 +4,12 @@ export const protect = (req, res, next) => {
   const token = req.headers.authorization?.split("")[1];
 
   if (!token) return res.status(401).json({ message: "No token" });
-};
 
-try {
-  const decoded = jwt.verify(token, process.env.JWT_SECRET);
-  req.userId = decoded.userId;
-  next();
-} catch {
-  res.status(401).json({ message: "Invalid token" });
-}
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.userId = decoded.userId;
+    next();
+  } catch {
+    res.status(401).json({ message: "Invalid token" });
+  }
+};
