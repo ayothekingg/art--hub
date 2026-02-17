fdf0b75018c3299333c4f657b8d623cbd682ea80
Tobiloba Ayomide <oyetunjitobiloba82@gmail.com>
2026-02-17 19:07:52 +0100

    feat: add ScrollToTop component for improved navigation experience

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,10 +3,12 @@ import '@/App.css';
 import { appRoutes } from './routes';
 import Register from '@/pages/auth/Register';
 import Login from '@/pages/auth/Login';
+import ScrollToTop from '@/components/shared/ScrollToTop';
 
 function App() {
   return (
     <BrowserRouter>
+      <ScrollToTop />
       <Routes>
         {appRoutes.map(route => (
           <Route key={route.path} path={route.path} element={route.element} />
